839248cb85eec8db8d438cacbb5e30711e12062b
Cecilia Paiva <cilhapaiva29@gmal.com>
2023-01-14 15:54:32 -0300

    primeiro de muitos de 2k23 initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMode() {
+const html = document.documentElement
+
+//if (html.classlist.contains('lightMode')) {
+//html.classlist.remove('lightMode')
+//} else {
+//html.classlist.add('lightMode')
+// }
+html.classList.toggle("lightMode")}
